cdb723e14f0f68a5fed326f07c7bfbb5c75561e8
Vijay <vijay.r.harde@gmail.com>
2022-11-21 12:25:26 +0530

    typing effect

--- index.js
+++ index.js
@@ -1,28 +1,69 @@
-let input = "my name is abc";
-// ym eman si cba
-array = [];
-temp = "";
-for(let i = 0;i<=input.length;i++){
-    if(input[i] == " " || i == input.length){
-        array.push(temp);
-        temp = "";
-    }else {
-        temp = temp + input[i];
-    }
-}
-console.log(array)
+// let input = "my name is abc";
+// // ym eman si cba
+// array = [];
+// temp = "";
+// for(let i = 0;i<=input.length;i++){
+//     if(input[i] == " " || i == input.length){
+//         array.push(temp);
+//         temp = "";
+//     }else {
+//         temp = temp + input[i];
+//     }
+// }
+// console.log(array)
 
-function reverseString(string)  {
-    let strn = "";
-    for(let i = string.length -1 ; i>=0;i--){
-        strn =  strn + string[i];
-    }
-    return strn;
-}
-let answer = "";
- for(let i of array){
-    console.log(i)
-    answer += reverseString(i) + " ";
- }
+// function reverseString(string)  {
+//     let strn = "";
+//     for(let i = string.length -1 ; i>=0;i--){
+//         strn =  strn + string[i];
+//     }
+//     return strn;
+// }
+// let answer = "";
+//  for(let i of array){
+//     console.log(i)
+//     answer += reverseString(i) + " ";
+//  }
 
- console.log(answer)
+//  console.log(answer)
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["hard", "fun", "a journey", "LIFE"];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+  	setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+	if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
